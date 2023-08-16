5b0f7537c66566f983ba40061d269ec6d33af652
Nicky Irawan <niwasmala@gmail.com>
2023-08-16 17:07:43 +0700

    chore: Fix error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
   export let data;
+
+  let countries = data.countries ?? [];
 </script>
 
 <h1 class="text-3xl font-bold underline">
   Hello world!
 </h1>
 <ul>
-  {#each data.countries as country}
+  {#each countries as country}
     <li>{country.name}</li>
   {/each}
 </ul>
