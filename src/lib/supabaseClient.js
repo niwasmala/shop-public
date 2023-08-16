1c592244231cb3bdb8aa5fcd1a42b67fdb24d3ae
Nicky Irawan <niwasmala@gmail.com>
2023-08-16 17:12:30 +0700

    chore: Add supabase client

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+  import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://sydvxfmlcgdusxjnealn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5ZHZ4Zm1sY2dkdXN4am5lYWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIxNTkzMzgsImV4cCI6MjAwNzczNTMzOH0.vsqB2HaZA-2f7lpPhMl7z0ellrstd2cLWgduglKbrak')
