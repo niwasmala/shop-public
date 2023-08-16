c468e9d25f009ee4fd210582436df1f39a9f6af3
Nicky Irawan <niwasmala@gmail.com>
2023-08-16 16:59:05 +0700

    chore: Add supabase, page server, and netlify build setting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
+<script>
+  export let data;
+</script>
+
 <h1 class="text-3xl font-bold underline">
   Hello world!
 </h1>
+<ul>
+  {#each data.countries as country}
+    <li>{country.name}</li>
+  {/each}
+</ul>
 
 <style lang="postcss">
   :global(html) {
